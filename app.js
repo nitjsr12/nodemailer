0b68a225b5be4ff140cdee3d30c37f961de24831
RAVI  kumar <40166356+nitjsr12@users.noreply.github.com>
2020-01-01 23:18:03 +0530

    intial

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
     //__dirname : It will resolve to your project folder.
 });
 
